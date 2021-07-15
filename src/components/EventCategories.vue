55f47c789bf67369f18612c5ce9fa8a1fbf6ef44
Pongpanoth Panya <pongpanoth213@gmail.com>
2021-07-16 03:09:30 +0700

    3.12

---
+++ src/components/EventCategories.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="event-categories">
+    <h5>Category: {{ event.category }}</h5>
+    <h5>Organizer: {{ event.organizer }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCategories',
+  // props: {
+  // msg: String
+  // }
+
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-categories {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
